567ded9850c8946631c8c62006aaa7799308a343
Akashakke <98536933+Akashakke@users.noreply.github.com>
2022-03-02 23:54:36 +0530

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,16 @@
+var request =new XMLHttpRequest();
+ request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+     console.log(data);
+     var res=data.filter((element)=>element.region==="Asia");
+var final=res.map((ele)=>ele.population);
+var final1=final.reduce((sum,cv)=>sum+cv);
+console.log(`The Total population of all countries are ${final1}`)
+
+}
+
+
+
+
